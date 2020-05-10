b9e9b190f157d084db6f3e2a5fa5e3b44af6392f
Hans Kristian Smedsrød <eplehans@gmail.com>
2020-05-10 09:43:22 +0200

    removed fancy scroll

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -10,31 +10,6 @@ var isTouchDevice = function () {
     );
 };
 
-
-$.fn.moveIt = function(){
-    var $window = $(window);
-    var instances = [];
-    
-    $(this).each(function(){
-      instances.push(new moveItItem($(this)));
-    });
-    
-    window.addEventListener('scroll', function(){
-      var scrollTop = $window.scrollTop();
-      instances.forEach(function(inst){
-        inst.update(scrollTop);
-      });
-    }, {passive: true});
-  }
-  
-  var moveItItem = function(el){
-    this.el = $(el);
-    this.speed = parseInt(this.el.attr('data-scroll-speed'));
-  };
-  
-  moveItItem.prototype.update = function(scrollTop){
-    this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
-  };
   
 
 function init(){
@@ -93,13 +68,7 @@ window.onload = function () {
     
     var touchClass = isTouchDevice() ? "touch" : "no-touch";
     document.body.classList.add(touchClass);
-
-    if(!isTouchDevice() && $(window).width() >= 900 ){
-        $(function(){
-            $('[data-scroll-speed]').moveIt();
-        });
-    }
-
+    
     document.body.classList.add("loaded");
 
 }
